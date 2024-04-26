c1fa8dea43e1d03604fd756f1c69c7c755045792
LamboCreeper <12111454+LamboCreeper@users.noreply.github.com>
2024-04-26 22:19:31 +0100

    Setup Firebase for React

--- hosting/src/index.tsx
+++ hosting/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./firebase";
 
 const root = ReactDOM.createRoot(
   document.getElementById("app") as HTMLElement
