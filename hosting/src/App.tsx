6ea3d4263f44a2790f6054ff90e63e965dcfe892
LamboCreeper <12111454+LamboCreeper@users.noreply.github.com>
2024-04-27 01:39:09 +0100

    Setup Firebase with React and basic auth implementation to test it

--- hosting/src/App.tsx
+++ hosting/src/App.tsx
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { User, onAuthStateChanged, GithubAuthProvider, signInWithRedirect, getAuth, signOut } from "firebase/auth";
+import { app } from "./firebase";
+
+const auth = getAuth(app);
 
 function App() {
-  return (
-      <main>
-        <h1>
-          CodeSupport Code Review Tool
-        </h1>
-      </main>
-  );
+    const [user, setUser] = useState<User | undefined>();
+
+    useEffect(() => {
+        const listener = onAuthStateChanged(auth, (user) => {
+            setUser(user ?? undefined);
+        });
+
+        return () => listener();
+    }, []);
+
+    const handleLogin = async () => {
+        const provider = new GithubAuthProvider();
+
+        provider.addScope("read:user");
+        provider.addScope("public_repo");
+
+        await signInWithRedirect(auth, provider);
+    };
+
+    const handleLogout = () => {
+        signOut(auth);
+    };
+
+    return (
+        <main>
+            <h1>
+                CodeSupport Code Review Tool
+            </h1>
+            {user && (
+                <div>
+                    <p>Hello {user.displayName}</p>
+                    <pre>{JSON.stringify(user)}</pre>
+                    <button onClick={handleLogout}>
+                        Log Out
+                    </button>
+                </div>
+            )}
+            {!user && (
+                <button onClick={handleLogin}>
+                    Login
+                </button>
+            )}
+        </main>
+    );
 }
 
 export default App;
---
+++ hosting/src/firebase.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+
+export const app = initializeApp({
+	apiKey: "AIzaSyBZh6KFSTk8Ue33Lve7CETgJU9JpTfBAJg",
+	authDomain: "codesupport.dev",
+	databaseURL: "https://codesupport-production.firebaseio.com",
+	projectId: "codesupport-production",
+	storageBucket: "codesupport-production.appspot.com",
+	messagingSenderId: "300111604570",
+	appId: "1:300111604570:web:39ae8baff812ed26fe2163",
+	measurementId: "G-PRJWCPYV56"
+});
+
+const emulatorDetails = {
+	authURL: process.env.REACT_APP_FIREBASE_AUTH_EMULATOR_URL,
+	firestoreHost: process.env.REACT_APP_FIREBASE_FIRESTORE_EMULATOR_HOST,
+	firestorePort: process.env.REACT_APP_FIREBASE_FIRESTORE_EMULATOR_PORT
+};
+
+if (process.env.REACT_APP_ENV !== "production") {
+	connectAuthEmulator(getAuth(), emulatorDetails.authURL!);
+	connectFirestoreEmulator(getFirestore(), emulatorDetails.firestoreHost!, +emulatorDetails.firestorePort!);
+}
