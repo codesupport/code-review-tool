c0ebdfa2ad1844f53bbe4a85b2ef5310221da1df
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2024-04-27 01:12:05 +0100

    Basic React App (#2)
    * Basic Firebase setup
    
    * Basic React app setup

---
+++ hosting/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+      <main>
+        <h1>
+          CodeSupport Code Review Tool
+        </h1>
+      </main>
+  );
+}
+
+export default App;
