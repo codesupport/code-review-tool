cc02ee9ddbbe198a53a79f8354dd427760f6efc9
LamboCreeper <12111454+LamboCreeper@users.noreply.github.com>
2024-05-04 16:19:35 +0100

    Set access token in session

--- hosting/src/hooks/useUser.ts
+++ hosting/src/hooks/useUser.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
+import { getAuth, onAuthStateChanged, getRedirectResult, GithubAuthProvider, type User } from "firebase/auth";
 import { app } from "../firebase";
 
 const auth = getAuth(app);
@@ -8,6 +8,16 @@ export function useUser() {
 	const [user, setUser] = useState<User | undefined>();
 
 	useEffect(() => {
+		(async () => {
+			const result = await getRedirectResult(auth);
+
+			if (!result) return;
+
+			const credential = GithubAuthProvider.credentialFromResult(result);
+
+			sessionStorage.setItem("github_token", process.env.REACT_APP_LOCAL_GITHUB_TOKEN ?? credential!.accessToken!);
+		})();
+
 		const listener = onAuthStateChanged(auth, (user) => {
 			setUser(user ?? undefined);
 		});
