16a9cf4d80d4b3e8d11134da9d441dba541f4b55
LamboCreeper <12111454+LamboCreeper@users.noreply.github.com>
2024-06-02 17:57:43 +0100

    List a user's imported repositories

--- hosting/src/components/pages/HomePage.tsx
+++ hosting/src/components/pages/HomePage.tsx
@@ -1,8 +1,17 @@
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import { useUser } from "../../hooks/useUser";
+import { gitRepositoryRepository } from "../../repositories/GitRepositoryRepository";
+import { formatGitUrl } from "../../utils/GitUtils";
 
 export function HomePage() {
     const [user] = useUser();
+
+    const { data: importedRepositories } = useQuery({
+        queryKey: ["db", "crt_repositories", user?.uid],
+        queryFn: async () => gitRepositoryRepository.getByUserId(user!.uid),
+        enabled: !!user
+    });
 
     return (
         <main>
@@ -14,8 +23,23 @@ export function HomePage() {
                     <h1>Hello {user.displayName}</h1>
                     <pre>{JSON.stringify(user)}</pre>
                     <h2>Repositories</h2>
-                    <p>You have not yet imported any repositories.</p>
-                    <Link to="repositories/import">Import One</Link>
+                    {importedRepositories?.length ? (
+                        <>
+                            <ul>
+                                {importedRepositories.map(repo => (
+                                    <li key={repo.id}>
+                                        <Link to={`repositories/review/${repo.id}`}>{formatGitUrl(repo.git_url)} ({repo.commit})</Link>
+                                    </li>
+                                ))}
+                            </ul>
+                            <Link to="repositories/import">Import More</Link>
+                        </>
+                    ) : (
+                        <>
+                            <p>You have not yet imported any repositories.</p>
+                            <Link to="repositories/import">Import One</Link>
+                        </>
+                    )}
                 </div>
             )}
         </main>
