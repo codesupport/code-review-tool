f3afac1d3f3ab86bfbdd2ab2723d6789db5173bb
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2024-07-11 21:42:10 +0100

    No Duplicate Repository Imports (#23)

--- hosting/src/repositories/GitRepositoryRepository.ts
+++ hosting/src/repositories/GitRepositoryRepository.ts
@@ -5,6 +5,14 @@ export class GitRepositoryRepository extends BaseRepository<GitRepositoryModel> 
 	constructor() {
 		super("crt_repositories");
 	}
+
+	async create(data: GitRepositoryModel): Promise<string> {
+		const documentId = await super.create(data);
+
+		await this.createUniqueField(documentId, "hash", data.hash);
+
+		return documentId;
+	}
 }
 
 export const gitRepositoryRepository = new GitRepositoryRepository();
